e9bd01a784a56d8ba98766c8ba2f6cfb76ace037
punker76 <punkerat76@gmail.com>
2020-03-18 18:03:46 +0100

    SyncThemeWithWindowsAppModeSetting

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -5,7 +5,14 @@ namespace MahApps.Metro.netcoreapp30
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
     {
+        base.OnStartup(e);
+
+        ThemeManager.IsAutomaticWindowsAppModeSettingSyncEnabled = true;
+        ThemeManager.SyncThemeWithWindowsAppModeSetting();
     }
+}
 }
